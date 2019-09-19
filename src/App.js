ff4e48934bfc139e684cd3346e2e6d47a5b18920
Sarah <sweerna1@gmail.com>
2019-09-19 12:00:30 +0300

    TEXT Editor

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ const styles = {
   bold: { fontWeight: "bold" },
   italic: { fontStyle: "italic" },
   underline: { textDecorationLine: "underline" }
+  //currentcolor: "black"
 };
 
 class App extends Component {
+  state = {
+    currentcolor: "black",
+    bold: false,
+    italic: false,
+    underline: false
+  };
+
+  //function text
+  selectColor = color => {
+    this.setState({ currentcolor: color });
+  };
+
+  selectStyle = style => {
+    this.setState({ [style]: !this.state[style] });
+  };
+
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => this.selectStyle(style)}
+        >
           {style}
         </button>
       );
@@ -25,6 +46,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.selectColor(color)}
         />
       );
     });
@@ -32,7 +54,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.currentcolor,
+            fontWeight: this.state.bold ? "bold" : "",
+            fontStyle: this.state.italic ? "italic" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
